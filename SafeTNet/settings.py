176b563b1fd95b87c245f872987bb039f6c92fd8
mayur533 <mayurkhalate63@gmail.com>
2025-10-29 15:41:08 +0530

    Add safetnet-backend.onrender.com to ALLOWED_HOSTS

--- SafeTNet/settings.py
+++ SafeTNet/settings.py
@@ -30,6 +30,7 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 ALLOWED_HOSTS.append('safetnet.onrender.com')
+ALLOWED_HOSTS.append('safetnet-backend.onrender.com')
 # Application definition
 
 INSTALLED_APPS = [
