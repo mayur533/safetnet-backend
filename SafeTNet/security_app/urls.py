0bdc7066dc9facd7578b9114b40752e53cc0b1ac
Aditya Babar <adityabuzz331@gmail.com>
2025-11-05 16:29:00 +0530

    navigation api usage comment in urls.py

--- SafeTNet/security_app/urls.py
+++ SafeTNet/security_app/urls.py
@@ -8,7 +8,7 @@ router.register(r'case', views.CaseViewSet, basename='security-case')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('navigation/', views.NavigationView.as_view(), name='security-navigation'),
+    path('navigation/', views.NavigationView.as_view(), name='security-navigation'),#http://localhost:8000/api/security/navigation/?from_lat=18.5204&from_lng=73.8567&to_lat=18.5310&to_lng=73.8440
     path('incidents/', views.IncidentsView.as_view(), name='security-incidents'),
     path('login/', views.OfficerLoginView.as_view(), name='security-login'),
     path('profile/', views.OfficerProfileView.as_view(), name='security-profile'),
